4d7237661f7e32cc2fc3d91600f85361067101f5
Nikita Kuznetsov <nikita0bond@gmail.com>
2020-03-23 21:38:30 +0300

    Add some realizations for Monom

--- lab4/polinomial_lib/monomial.cpp
+++ lab4/polinomial_lib/monomial.cpp
@@ -51,3 +51,78 @@ Monom::~Monom()
 	coeff = 0;
 	next = 0;
 }
+
+void Monom::setNext(Monom* _next)
+{
+	next = _next;
+}
+
+void Monom::setPower(int* _power)
+{
+	for (int i = 0; i < n; i++)
+		if (_power[i] >= 0)
+			power[i] = _power[i];
+		else
+			throw - 1;
+}
+
+void Monom::setCoeff(double _coeff)
+{
+	coeff = _coeff;
+}
+
+void Monom::setN(int _n)
+{
+	if (_n < 0)
+		throw "Size is lower than 0";
+	else
+		if (_n == 0)
+		{
+			if (power != 0)
+				delete[] power;
+			n = 0;
+			power = 0;
+		}
+		else
+		{
+			int* tmp = new int[n];
+			for (int i = 0; i < n; i++)
+				tmp[i] = power[i];
+			delete[] power;
+			int i = 0;
+			power = new int[_n];
+			if (n < _n)
+			{
+				for (; i < n; i++)
+					power[i] = tmp[i];
+				for (; i < _n; i++)
+					power[i] = 0;
+			}
+			if (_n < n)
+				for (; i < _n; i++)
+					power[i] = tmp[i];
+			n = _n;
+		}
+}
+
+Monom* Monom::getNext()
+{
+	return next;
+}
+
+int* Monom::getPower()
+{
+	return power;
+}
+
+double Monom::getCoeff()
+{
+	return coeff;
+}
+
+int Monom::getN()
+{
+	return n;
+}
+
+
