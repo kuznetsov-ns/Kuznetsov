ef99eae10ac61233b012ccd0e8349039d9e11d96
Nikita Kuznetsov <nikita0bond@gmail.com>
2020-03-23 21:15:33 +0300

    Add class Monom

--- lab4/polinomial_lib/monomial.h
+++ lab4/polinomial_lib/monomial.h
@@ -1,2 +1,38 @@
 #pragma once
+#include <iostream>
 
+using namespace std;
+
+class Monom
+{
+protected:
+	Monom* next;
+	int* power;
+	int n;
+	double coeff;
+public:
+	Monom();
+	Monom(int _n, int* _power, double _coeff);
+	Monom(Monom& a);
+	virtual ~Monom();
+
+	void setPower(int* _power);
+	void setNext(Monom* _next);
+	void setCoeff(double _coeff);
+	void setN(int _n);
+	int* getPower();
+	Monom* getNext();
+	double getCoeff();
+	int getN();
+
+	Monom& operator=(Monom& a);
+	Monom& operator+(Monom& a);
+	Monom& operator+=(Monom& a);
+	Monom& operator-(Monom& a);
+	Monom& operator-=(Monom& a);
+	Monom& operator*(Monom& a);
+	Monom& operator*=(Monom& a);
+	bool operator==(Monom& a);
+	bool operator>(Monom& a);
+	bool operator<(Monom& a);
+};
