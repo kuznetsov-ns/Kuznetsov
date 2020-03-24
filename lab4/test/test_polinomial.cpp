719e24926241b96201aef6f426394edbe6cb8f16
Nikita Kuznetsov <nikita0bond@gmail.com>
2020-03-24 15:54:49 +0300

    Start test realization for Monom class

--- lab4/test/test_polinomial.cpp
+++ lab4/test/test_polinomial.cpp
@@ -1,4 +1,20 @@
 #include "polinomial.h"
-
 #include <gtest.h>
+
+TEST(Monom, can_create_monomial)
+{
+	ASSERT_NO_THROW(Monom mon);
+}
+
+TEST(Monom, can_create_monomial_with_positive_lenght)
+{
+	int arr[] = { 1,2,3 };
+	ASSERT_NO_THROW(Monom mon(3, arr, 4));
+}
+
+TEST(Monom, throws_when_create_monomial_with_negative_size)
+{
+	int arr[] = { 1,2,3 };
+	ASSERT_ANY_THROW(Monom mon(-2, arr, 2));
+}
 
